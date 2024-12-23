7a4dcbf0735f1e23136b60437b16b9135948199b
Vishnu <iam.vishnu.bhaskar@gmail.com>
2024-12-23 14:02:46 +0530

    Started woring on Custom build function for crawl site for sheetReport page.

--- src/Components/SheetReportPage/SheetReportForm.tsx
+++ src/Components/SheetReportPage/SheetReportForm.tsx
@@ -1,12 +1,20 @@
+'use client';
+
 import { RiGlobalLine } from "@remixicon/react"
+import { useState } from "react"
 
 const SheetReportForm = () => {
+
+    const [baseUrl, setBaseUrl] = useState<string>('')
+
     return (
         <form
             className="flex gap-2 items-center flex-col md:flex-row w-full md:w-max">
             <div className="w-full md:w-max flex gap-2 items-center bg-white py-2 px-4 rounded-lg">
                 <RiGlobalLine size={23} color="#00000030" />
                 <input
+                    value={baseUrl}
+                    onChange={(e) => setBaseUrl(e.target.value)}
                     type="text"
                     required
                     className="outline-none"
