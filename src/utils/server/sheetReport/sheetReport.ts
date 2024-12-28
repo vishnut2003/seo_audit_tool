668cc53331c107eae44e599c2af1b5486845cb5a
Vishnu <iam.vishnu.bhaskar@gmail.com>
2024-12-28 17:00:26 +0530

    SheetReportPage: Configured puppeteer for aws lambda server.

--- src/utils/server/sheetReport/sheetReport.ts
+++ src/utils/server/sheetReport/sheetReport.ts
@@ -2,9 +2,11 @@ import { fetchSitemap } from "./createSheetReport/fetchSitemap"
 import { checkTitleLessThat30 } from "./createSheetReport/titleChecks";
 import { ForSheetGroupInterface, titileLessThan30Interface } from "./sheetReportInterfaces";
 import puppeteer from "puppeteer";
+import puppeteer_core from "puppeteer-core";
 import updateTotalPage from "./databaseActions/updateTotalPage";
 import updateFinishPage from "./databaseActions/updateFinishPage";
 import updateStatus from "./databaseActions/updateStatus";
+import chromium from "@sparticuz/chromium";
 
 export async function createSheetReport({ baseUrl, reportId }: {
     baseUrl: string,
@@ -27,16 +29,31 @@ export async function createSheetReport({ baseUrl, reportId }: {
 
             // Lauch puppeteer browser
             console.log("lauching browser!")
-            const browser = await puppeteer.launch({headless: true});
+
+            let browser;
+
+            if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+                console.log('Assigning browser for AWS Lambda!')
+                browser = await puppeteer_core.launch({
+                    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+                    defaultViewport: chromium.defaultViewport,
+                    executablePath: await chromium.executablePath(),
+                    headless: true,
+                })
+            } else {
+                console.log('Assigning browser for local run!')
+                browser = await puppeteer.launch()
+            }
+
             const page = await browser.newPage();
 
             for (const url of pagesList) {
                 console.log(`Opening ${url}`)
-                await page.goto(url, {timeout: 0});
+                await page.goto(url, { timeout: 0 });
 
                 // check page title
                 const pageTitle = await page.title();
-                const failedTitleL30check = await checkTitleLessThat30({title: pageTitle, url});
+                const failedTitleL30check = await checkTitleLessThat30({ title: pageTitle, url });
                 if (failedTitleL30check) {
                     titileLessThan30.push(failedTitleL30check);
                 }
