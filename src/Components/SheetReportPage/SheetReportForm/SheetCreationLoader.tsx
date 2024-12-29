2714f5ea60d73f1f7debb4356c55726d747b5790
Vishnu <iam.vishnu.bhaskar@gmail.com>
2024-12-29 14:21:58 +0530

    SheetReportPage: added progress data to SheetCreationLoader popup.

--- src/Components/SheetReportPage/SheetReportForm/SheetCreationLoader.tsx
+++ src/Components/SheetReportPage/SheetReportForm/SheetCreationLoader.tsx
@@ -1,24 +1,52 @@
+'use client';
+
 import TripleDotLoading from "@/Components/Loaders/TripleDotLoading/TripleDotLoading"
+import { RiRefreshLine } from "@remixicon/react";
+import { useState } from "react";
+
+export interface ProgressLoaderDataInterface {
+  totalPages: number,
+  finishPage: number,
+  siteUrl: string,
+}
 
 const SheetCreationLoader = () => {
+
+  const [progressData, setProgressData] = useState<ProgressLoaderDataInterface | false>(false);
+
   return (
     <div className="fixed top-0 left-0 w-full h-full p-5 bg-[#ffffff70] flex justify-center items-center z-50">
-        <div className="p-5 bg-white rounded-xl drop-shadow-2xl max-w-screen-sm w-full max-h-[500px] h-full flex flex-col justify-center items-center gap-3">
-            <TripleDotLoading/>
-            <h2>Creating Google Sheet...</h2>
+      <div className="p-5 bg-white rounded-xl drop-shadow-2xl max-w-screen-sm w-full max-h-[500px] h-full flex flex-col justify-center items-center gap-3">
+        <TripleDotLoading />
+        <h2>Creating Google Sheet...</h2>
 
-            <div className="mt-3 text-center flex flex-col gap-1">
-                <p className="text-sm">Creating report can take more time based on pages.</p>
-                <p className="text-sm text-red-400">Do not close this tab, or report will fail</p>
-            </div>
-
-            <a 
-            href='/dashboard/sheet-reports' 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="text-primary font-semibold"
-            >View Progress</a>
+        <div className="mt-3 text-center flex flex-col gap-1">
+          <p className="text-sm">Creating report can take more time based on pages.</p>
+          <p className="text-sm text-red-400">Do not close this tab, or report will fail</p>
         </div>
+
+        {/* progress details */}
+
+        <div className="flex flex-col gap-2 justify-center items-center">
+          <h2 className="text-lg mb-2 text-center font-semibold">Crawling in progress</h2>
+          {
+            !progressData ? 
+            <p>Might take few seconds for updates...</p> : 
+            <div className="text-center">
+              <p>{progressData.siteUrl}</p>
+              <p>Pages <strong>{progressData.finishPage}</strong> finished of <strong>{progressData.totalPages}</strong></p>
+            </div>
+          }
+          <button
+          type="button"
+          className="flex justify-center items-center gap-1 mt-2 bg-gray-100 active:bg-gray-300 rounded-md py-3 px-5 shadow-md"
+          >
+            <RiRefreshLine size={15}/>
+            <p className="font-semibold">Recheck</p>
+          </button>
+        </div>
+
+      </div>
     </div>
   )
 }
