c6be55865add113e763c62984a2a49f65bfbc528
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-01-09 19:45:15 +0530

    Remove console log statements from canonical URL check in competitor analysis

--- src/utils/server/competitorAnalysis/competitorAnalysisReport/crawlParts/canonicalCheck.ts
+++ src/utils/server/competitorAnalysis/competitorAnalysisReport/crawlParts/canonicalCheck.ts
@@ -11,12 +11,10 @@ export async function checkMainDomainCanonical({ DOM, page, url }: {
             // fetch current page url
             await page.goto(url)
             const currentUrl = page.url();
-            console.log(currentUrl);
 
             // fetch canonical url
             const linkElement: HTMLLinkElement | null = DOM.window.document.querySelector("head > link[rel='canonical']");
             const canonicalUrl: string = linkElement ? linkElement.href : "";
-            console.log(canonicalUrl);
 
             // check if canonical issue exist
             if (currentUrl === canonicalUrl) {
