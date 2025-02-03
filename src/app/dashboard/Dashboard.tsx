5c038a3779769c85aab979a1e3654ff6219102dc
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-02-03 18:16:03 +0530

    Refactor dashboard layout: adjust spacing and grid configuration for improved responsiveness

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -11,11 +11,11 @@ const Dashboard = ({ currentProject }: {
             className="space-y-7"
         >
             <div
-                className="flex items-center gap-5"
+                className="flex items-center gap-1 md:gap-5"
             >
                 <Link
                     href={'/dashboard/projects'}
-                    className="text-sm py-3 px-5 flex justify-center items-center gap-2 bg-white font-medium rounded-md shadow-xl shadow-gray-200"
+                    className="text-sm text-nowrap py-2 md:py-3 px-4 md:px-5 flex justify-center items-center gap-2 bg-white font-medium rounded-md shadow-xl shadow-gray-200"
                 >
                     <RiArrowLeftSLine
                         size={20}
@@ -49,7 +49,7 @@ const Dashboard = ({ currentProject }: {
 
                 {/* Left Col */}
                 <div
-                    className="w-full grid grid-cols-4 gap-4"
+                    className="w-full grid grid-cols-1 md:grid-cols-4 gap-4"
                 >
 
                     {/* PDF Report Option */}
