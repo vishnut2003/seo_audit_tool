966df275320d8e5f5d5e613c79fa2b1cc07cc8aa
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-02-25 18:31:00 +0530

    Implement loading state table for projects with animated cells for better user experience

--- src/app/dashboard/projects/LoadingProjectsTemplate.tsx
+++ src/app/dashboard/projects/LoadingProjectsTemplate.tsx
@@ -1,12 +1,57 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/Components/ui/table"
+import { RiArrowUpDownLine } from "@remixicon/react"
 
 const LoadingProjectsTemplate = () => {
     return (
-        <div
-            className="p-4 min-h-[400px] flex justify-center items-center"
+        <Table
+            className="space-y-4"
         >
-            <p>Please wait...</p>
-        </div>
+            <TableHeader>
+                <TableRow>
+                    <TableHead></TableHead>
+                    <TableHead className="items-center">
+                        Date
+                        <RiArrowUpDownLine
+                            size={15}
+                            className="inline ml-2"
+                        />
+                    </TableHead>
+                    <TableHead>Domain</TableHead>
+                    <TableHead>Last Updated</TableHead>
+                    <TableHead>No. of Competitors</TableHead>
+                    <TableHead>Actions</TableHead>
+                </TableRow>
+            </TableHeader>
+
+            {/* Table Body */}
+            <TableBody>
+
+                {
+                    [1, 2, 3, 4, 5, 6].map((row) => (
+                        <TableRow
+                            key={row}
+                        >
+                            {
+                                [1, 2, 3, 4, 5, 6].map((col) => (
+                                    <TableCell
+                                        className="w-[50px] h-14"
+                                        key={col}
+                                    >
+                                        <AnimateTableCell/>
+                                    </TableCell>
+                                ))
+                            }
+                        </TableRow>
+                    ))
+                }
+
+            </TableBody>
+        </Table>
     )
+}
+
+function AnimateTableCell() {
+    return <div className="animate-pulse bg-gray-200 h-4 w-full rounded"></div>
 }
 
 export default LoadingProjectsTemplate
