56bff550ac9a35fba47f70e9d0844cc6dea38411
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-04-23 13:15:55 +0530

    feat: update ConvertorPage to use SingleToolsLayout with title and description

--- src/app/seo-tools/(tools)/(Convertor)/convertor/page.tsx
+++ src/app/seo-tools/(tools)/(Convertor)/convertor/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import SingleToolsLayout from '../../LayoutTemplate'
+
+const PAGE_TITLE = "Convert to anything";
+const PAGE_DESC = "Convert any image or document file effortlessly into your desired format with speed, precision, and ease."
 
 const ConvertorPage = () => {
   return (
-    <div>ConvertorPage</div>
+    <SingleToolsLayout
+      pageTitle={PAGE_TITLE}
+      pageDesc={PAGE_DESC}
+    >
+      test
+    </SingleToolsLayout>
   )
 }
 
