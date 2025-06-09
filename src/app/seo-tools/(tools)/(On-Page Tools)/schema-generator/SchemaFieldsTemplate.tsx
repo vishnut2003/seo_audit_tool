32132efe4e95c6c754970289f19612e1978de00a
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-06-09 13:11:51 +0530

    Schema generator -> Article Schema finsh.

--- src/app/seo-tools/(tools)/(On-Page Tools)/schema-generator/SchemaFieldsTemplate.tsx
+++ src/app/seo-tools/(tools)/(On-Page Tools)/schema-generator/SchemaFieldsTemplate.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { SchemaTypesListInterface, SchemaTypesListJsonReturnFunctionArgumentType } from './schemaTypesList'
 
 import {
@@ -12,6 +12,8 @@ import {
 
 import { Input } from "@/Components/ui/input"
 import { Label } from "@/Components/ui/label"
+import { RiAddLargeLine, RiCloseLargeLine } from '@remixicon/react'
+import DatePicker from '@/Components/ui/datepicker'
 
 const SchemaFieldsTemplate = ({
     selectedSchemaType,
@@ -28,32 +30,62 @@ const SchemaFieldsTemplate = ({
                 className='flex flex-wrap w-full gap-3 justify-between'
             >
                 {
-                    selectedSchemaType.enableFields.map((field, index) => (
-                        <div
-                            key={index}
-                            className={`w-[${field.width}]`}
-                        >
-                            {
-                                field.type === "dropdown" &&
-                                <DropdownTemplate
-                                    placeholder={field.placeholder}
-                                    label={field.label}
-                                    items={field.dropdownList || []}
-                                    name={field.name}
-                                    setFieldData={setFieldsData}
-                                />
-                            }
-                            {
-                                field.type === "text" &&
-                                <TextFieldTemplate
-                                    label={field.label}
-                                    name={field.name}
-                                    placeholder={field.placeholder}
-                                    setFieldData={setFieldsData}
-                                />
-                            }
-                        </div>
-                    ))
+                    selectedSchemaType.enableFields.map((field, index) => {
+
+                        if (typeof field === "string") {
+                            return (
+                                <h2
+                                    className='text-lg font-semibold text-themesecondary w-full'
+                                    key={index}
+                                >{field}</h2>
+                            )
+                        }
+
+                        return (
+                            <div
+                                key={index}
+                                className={`w-[${field.width}]`}
+                            >
+                                {
+                                    field.type === "dropdown" &&
+                                    <DropdownTemplate
+                                        placeholder={field.placeholder}
+                                        label={field.label}
+                                        items={field.dropdownList || []}
+                                        name={field.name}
+                                        setFieldData={setFieldsData}
+                                    />
+                                }
+                                {
+                                    field.type === "text" &&
+                                    <TextFieldTemplate
+                                        label={field.label}
+                                        name={field.name}
+                                        placeholder={field.placeholder}
+                                        setFieldData={setFieldsData}
+                                    />
+                                }
+                                {
+                                    field.type === "list" &&
+                                    <ListFieldTemplate
+                                        label={field.label}
+                                        name={field.name}
+                                        placeholder={field.placeholder}
+                                        setFieldData={setFieldsData}
+                                    />
+                                }
+                                {
+                                    field.type === "date" &&
+                                    <DateFieldTemplate
+                                        label={field.label}
+                                        name={field.name}
+                                        placeholder={field.placeholder}
+                                        setFieldData={setFieldsData}
+                                    />
+                                }
+                            </div>
+                        )
+                    })
                 }
             </div>
         </div>
@@ -79,7 +111,7 @@ function DropdownTemplate({
         >
             <Label>{label}</Label>
             <Select
-                onValueChange={(value) => setFieldData(prev => ({...prev, [name]: value}))}
+                onValueChange={(value) => setFieldData(prev => ({ ...prev, [name]: value }))}
             >
                 <SelectTrigger className="w-[100%] h-9 border shadow-none border-gray-200">
                     <SelectValue placeholder={placeholder} />
@@ -121,7 +153,154 @@ function TextFieldTemplate({
                 id={name}
                 type="text"
                 placeholder={placeholder}
-                onChange={(e) => setFieldData(prev => ({...prev, [name]: e.target.value}))}
+                onChange={(e) => setFieldData(prev => ({ ...prev, [name]: e.target.value }))}
+            />
+        </div>
+    )
+}
+
+function ListFieldTemplate({
+    label,
+    name,
+    placeholder,
+    setFieldData,
+}: {
+    label: string,
+    placeholder: string,
+    name: string,
+    setFieldData: Dispatch<SetStateAction<SchemaTypesListJsonReturnFunctionArgumentType>>,
+}) {
+
+    const [fields, setFields] = useState([ListFieldSingleField]);
+
+    return (
+        <div>
+            <Label
+                htmlFor={name}
+            >{label}</Label>
+
+            <div
+                className='space-y-3'
+            >
+                {fields.map((Field, index) => (
+                    <Field
+                        index={index}
+                        key={index}
+                        placeHolder={placeholder}
+                        valueOnChange={(value) => {
+                            setFieldData(prev => {
+
+                                const listItems = Array.isArray(prev[name]) ? [...(prev[name] as string[])] : [];
+                                listItems[index] = value;
+
+                                return ({
+                                    ...prev,
+                                    [name]: listItems,
+                                })
+                            })
+                        }}
+                        deleteField={() => {
+                            const fieldListCopy = fields;
+                            fieldListCopy.splice(index, 1);
+                            setFields([...fieldListCopy]);
+
+                            setFieldData(prev => {
+                                const listItems = Array.isArray(prev[name]) ? [...(prev[name] as string[])] : [];
+                                listItems.splice(index, 1);
+                                return ({
+                                    ...prev,
+                                    [name]: listItems,
+                                })
+                            })
+                        }}
+                    />
+                ))}
+            </div>
+
+            <button
+                className='bg-themesecondary text-white text-sm flex items-center gap-3 rounded-md py-2 px-4 mt-3'
+                onClick={() => {
+                    setFields(prev => [...prev, ListFieldSingleField]);
+                }}
+            >
+                <RiAddLargeLine
+                    size={15}
+                />
+                Image
+            </button>
+        </div>
+    )
+}
+
+function ListFieldSingleField({
+    index,
+    placeHolder,
+    valueOnChange,
+    deleteField,
+}: {
+    index: number,
+    placeHolder: string,
+    valueOnChange: (value: string) => void,
+    deleteField: () => void,
+}) {
+    return (
+        <div
+            className='flex items-center gap-3'
+        >
+            <Input
+                type='text'
+                placeholder={`${placeHolder} #${index + 1}`}
+                onChange={(event) => {
+                    valueOnChange(event.target.value);
+                }}
+            />
+            {
+                index !== 0 &&
+                <button
+                    className='bg-red-50 text-red-500 p-2 rounded-md'
+                    onClick={() => deleteField()}
+                >
+                    <RiCloseLargeLine
+                        size={20}
+                    />
+                </button>
+            }
+        </div>
+    )
+}
+
+function DateFieldTemplate({
+    label,
+    name,
+    placeholder,
+    setFieldData,
+}: {
+    label: string,
+    placeholder: string,
+    name: string,
+    setFieldData: Dispatch<SetStateAction<SchemaTypesListJsonReturnFunctionArgumentType>>,
+}) {
+
+    const [date, setDate] = useState<Date | null>(null);
+
+    useEffect(() => {
+        const utcDate = date ? new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())) : null;
+        const dateString = utcDate ? utcDate.toISOString().split('T')[0] : "";
+        setFieldData(prev => ({
+            ...prev,
+            [name]: dateString,
+        }))
+    }, [date, name, setFieldData]);
+
+    return (
+        <div>
+            <Label
+                htmlFor={name}
+            >{label}</Label>
+            <DatePicker
+                date={date}
+                setDate={setDate}
+                placeholder={placeholder}
             />
         </div>
     )
--- src/app/seo-tools/(tools)/(On-Page Tools)/schema-generator/schemaTypesList.ts
+++ src/app/seo-tools/(tools)/(On-Page Tools)/schema-generator/schemaTypesList.ts
@@ -7,14 +7,14 @@ export interface SchemaTypesListJsonReturnFunctionArgumentType {
 export interface SchemaTypesListInterface {
     typeName: string,
     icon: RemixiconComponentType,
-    enableFields: {
-        type: "dropdown" | "text" | "list",
+    enableFields: ({
+        type: "dropdown" | "text" | "list" | "date",
         label: string,
         placeholder: string,
         name: string,
         dropdownList?: string[],
         width?: string,
-    }[],
+    } | string)[],
     returnJsonSchema: (fieldsValue: SchemaTypesListJsonReturnFunctionArgumentType) => string,
 }
 
@@ -49,39 +49,129 @@ const schemaTypesLists: SchemaTypesListInterface[] = [
                 name: "headline",
                 width: "100%",
             },
+            {
+                label: "Image URL(s)",
+                name: "images",
+                placeholder: "Image URL",
+                type: "list",
+                width: "100%",
+            },
+            {
+                label: "Short description of the article",
+                name: "shortDesc",
+                placeholder: "Short Description",
+                type: "text",
+                width: "100%",
+            },
+            "Author",
+            {
+                label: "Author @type",
+                name: "authorType",
+                placeholder: "Person/Organization",
+                type: "dropdown",
+                dropdownList: [
+                    "Person",
+                    "Organization",
+                ],
+                width: "45%",
+            },
+            {
+                label: "Author",
+                name: "author",
+                placeholder: "Author",
+                type: "text",
+                width: "45%",
+            },
+            {
+                label: "Author URL",
+                name: "authorUrl",
+                placeholder: "Author URL",
+                type: "text",
+                width: "100%",
+            },
+            "Publisher",
+            {
+                label: "Publisher",
+                name: "publisher",
+                placeholder: "Publisher",
+                type: "text",
+                width: "45%",
+            },
+            {
+                label: "Publisher Logo URL",
+                name: "publisherLogoUrl",
+                placeholder: "Publisher Logo URL",
+                type: "text",
+                width: "45%",
+            },
+            "Dates",
+            {
+                label: "Date Published",
+                name: "datePublished",
+                placeholder: "Date Published",
+                type: "date",
+                width: "45%",
+            },
+            {
+                label: "Date Modified",
+                name: "dateModified",
+                placeholder: "Date Modified",
+                type: "date",
+                width: "45%",
+            }
         ],
         returnJsonSchema: (fields) => {
 
-            const type = fields.type;
-            const url = fields.url;
-            const headline = fields.headline;
+            const type = fields.type || "";
+            const url = fields.url || "";
+            const headline = fields.headline || "";
+            const imageUrls = fields.images as string[] | undefined;
+            const shortDesc = fields.shortDesc || "";
+
+            // Author
+            const authorType = fields.authorType || "";
+            const author = fields.author || "";
+            const authorUrl = fields.authorUrl || "";
+
+            // Publisher
+            const publisher = fields.publisher || "";
+            const publisherLogoUrl = fields.publisherLogoUrl || "";
+
+            // Dates
+            const datePublished = fields.datePublished || "";
+            const dateModified = fields.dateModified || "";
 
             return (
                 `<script type="application/ld+json">
     {
     "@context": "https://schema.org",
-    "@type": "${type || 'Article'}",
-    ${
-    url &&
-    `"mainEntityOfPage": {
+    "@type": "${type || 'Article'}", ${url && `\n\t"mainEntityOfPage": {
         "@type": "WebPage",
         "@id": "${url}"
     },`}
-    "headline": "${headline}",
-    "image": "",  
+    "headline": "${headline || ""}", ${shortDesc && `\n\t"description": "${shortDesc}",`}
+    "image": ${!imageUrls ? `""` : imageUrls.length === 1 ? `"${imageUrls[0]}"` : `[
+        ${imageUrls.map((imageUrl, index) => {
+                    if (index === 0) {
+                        return `"${imageUrl}"`
+                    } else {
+                        return `\n \t\t"${imageUrl}"`
+                    }
+                })}
+    ]`},  
     "author": {
-        "@type": "",
-        "name": ""
+        "@type": "${authorType || ""}",
+        "name": "${author || ""}", ${authorUrl && `\n\t\t"url": "${authorUrl}"`}
     },  
     "publisher": {
             "@type": "Organization",
-            "name": "",
+            "name": "${publisher}",
             "logo": {
-            "@type": "ImageObject",
-            "url": ""
-        }
+                "@type": "ImageObject",
+                "url": "${publisherLogoUrl}"
+            }
     },
-    "datePublished": ""
+    "datePublished": "${datePublished}", ${dateModified && `\n\t"dateModified": "${dateModified}"`}
     }
 </script>`
             )
